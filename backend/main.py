ad4aad3a9ceb02cf22b65c9b6d81c2f1b2369401
NattaponS5 <natchan.ntpch@gmail.com>
2023-11-15 21:24:00 +0700

    main.py ver1

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,146 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from starlette.middleware.cors import CORSMiddleware
+from typing import Optional
+import mysql.connector
 
 app = FastAPI()
+
+# CORS (Cross-Origin Resource Sharing) middleware to allow communication with the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-class UserLogin(BaseModel):
-    username: str
-    password: str
+# MySQL database configuration
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="Roomie",
+)
+cursor = db.cursor()
 
+class User(BaseModel):
+    FirstName: str
+    LastName: str
+    Email: str
+    Username: str
+    Password: str
+
+@app.post("/signup")
+def signup(user: User):
+    try:
+        # Insert user data into the MySQL database
+        query = "INSERT INTO users (FirstName, LastName, Email, UserName, Password) VALUES (%s, %s, %s, %s, %s)"
+        values = (user.FirstName, user.LastName, user.Email, user.Username, user.Password)
+        cursor.execute(query, values)
+        db.commit()
+        return {"message": "User signed up successfully"}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+
+# Pydantic model for the login request
+class UserLogin(BaseModel):
+    Username: str
+    Password: str
+
+@app.post("/login")
+def login(user: UserLogin):
+    try:
+        # Check if the user exists in the MySQL database
+        query = "SELECT * FROM users WHERE Username = %s AND Password = %s"
+        values = (user.Username, user.Password)
+        cursor.execute(query, values)
+        result = cursor.fetchall()
+        if len(result) == 0:
+            return HTTPException(status_code=401, detail="Invalid credentials")
+        else:
+            return {"message": "User logged in successfully"}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+@app.get("/showuser")
+def users():
+    try:
+        # Get all users from the MySQL database
+        query = "SELECT * FROM users"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+# Pydantic model for the reservation request
+class booking(BaseModel):
+    day: int
+    month: str
+    year: int
+    start_time: str
+    end_time: str
+
+@app.post("/reservation")
+def reservation(reservation: Reservation):
+    try:
+        # Insert reservation data into the MySQL database
+        query = "INSERT INTO reservations (day, month, year, start_time, end_time) VALUES (%s, %s, %s, %s, %s)"
+        values = (reservation.day, reservation.month, reservation.year, reservation.start_time, reservation.end_time)
+        cursor.execute(query, values)
+        db.commit()
+        return {"message": "Reservation created successfully"}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/showreservation")
+def reservations():
+    try:
+        # Get all reservations from the MySQL database
+        query = "SELECT * FROM reservations"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+
+# Pydantic model for the room request
+class Room(BaseModel):
+    name: str
+    capacity: int
+    area: int
+    projector: bool
+    tv: bool
+    whiteboard: bool
+    video_conference: bool
+    description: str
+
+
+@app.post("/room")
+def room(room: Room):
+    try:
+        # Insert room data into the MySQL database
+        query = "INSERT INTO rooms (name, capacity, area , projector, tv, whiteboard, video_conference, description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (room.name, room.capacity, room.area, room.projector, room.tv, room.whiteboard, room.video_conference, room.description)
+        cursor.execute(query, values)
+        db.commit()
+        return {"message": "New Room created successfully"}
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/showroom")
+def rooms():
+    try:
+        # Get all rooms from the MySQL database
+        query = "SELECT * FROM rooms"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.post("/login")
-async def login(userlogin: UserLogin):
-    if userlogin.username == "admin" and userlogin.password == "password":
-        return {"username": "admin", "role": "admin"}
-    else:
-        return {"username": f"{userlogin.username}", "role": "user"}
--- frontend/src/routes/SignUp/+page.svelte
+++ frontend/src/routes/SignUp/+page.svelte
@@ -1,23 +1,49 @@
+<script>
+  import { onMount } from 'svelte';
 
+  let formData = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+</script>
 
 <div class="background h-3/4 pt-10 pb-32" id="get_in_touch">
-  <div id="form" class="pb-10 ">
+  <div id="form" class="pb-10">
     <p class="text-neutral text-5xl font-bold p-20 text-center">Sign Up</p>
-    <form>
+    <form on:submit|preventDefault={handleSubmit}>
       <div class="min-w-min mb-6">
-        <input type="text" name="name" id="name" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="First Name: " required>
+        <input bind:value={formData.first_name} type="text" name="name" id="name" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="First Name:" required>
       </div>
       <div class="min-w-min mb-6">
-        <input type="text" name="name" id="name" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Last Name: " required>
+        <input bind:value={formData.last_name} type="text" name="name" id="name" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Last Name:" required>
       </div>
       <div class="mb-6">
-        <input type="email" name="email" id="email" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Email: "required>
+        <input bind:value={formData.email} type="email" name="email" id="email" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Email:" required>
       </div>
       <div class="mb-6">
-        <input type="password" name="subject" id="subject" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Password: " required>
+        <input bind:value={formData.password} type="password" name="subject" id="subject" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Password:" required>
       </div>
-         <div class="mb-6">
-        <input type="password" name="subject" id="subject" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Confirm Password: " required>
+      <div class="mb-6">
+        <input type="password" name="subject" id="subject" class="bg-gray-50  text-sm rounded-lg  block w-full p-2.5  dark:text-black " placeholder="Confirm Password:" required>
       </div>
       <div class="mb-6 flex justify-center">
         <button type="submit" class="btn btn-accent md:max-w-fit text-white  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center  dark:hover:bg-accent-focus dark:focus:accent-focus" >Sign Up</button>
@@ -25,6 +51,7 @@
     </form>
   </div>
 </div>
+
 
 <style>
   .background{
